e1780a6acccd5f3f2e1c81189f7d5872d1119332
lucas taranne <lucas.tara960@gmail.com>
2023-05-17 10:05:09 +0100

    Test 3/3

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,14 +1,25 @@
 import React  from "react";
 
 function PokemonList({ searchTerm = "" }) {
+  const urlAPI = `https://pokeapi.co/api/v2/pokemon?limit=100`;
+  const [list, setList] = React.useState([]);
 
+  fetch(urlAPI)
+    .then((response) => response.json())
+    .then((data) => {
+      setList(data.results);
+    });
 
-  return (
-    <ul>
-    <li>test</li>
-    <li>test2</li>
-    </ul>
-  );
+    return (
+        <ul>
+            {list.filter((pokemon) => {
+                    return pokemon.name.includes(searchTerm);
+                })
+                  .map((pokemon) => (
+                    <li key={pokemon.name}>{pokemon.name}</li>
+            ))}
+        </ul>
+    );
 }
 
 export default PokemonList;
